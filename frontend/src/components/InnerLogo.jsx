f6731d3d964ccd6d7da68f9beda24a90c4e912fb
Pradhuman07 <upradyuman@gmail.com>
2025-08-02 02:39:37 +0530

    Header adjusted for desktop screens

--- frontend/src/components/InnerLogo.jsx
+++ frontend/src/components/InnerLogo.jsx
@@ -9,7 +9,7 @@ const InnerLogo = () => {
             <div className="w-7.5 h-7.5 lg:w-8 lg:h-8 bg-gradient-to-r from-blue-300 to-indigo-400 transform rotate-45 transition-transform flex items-center justify-center rounded-full">
                 <IoMdMusicalNote className="text-white transform -rotate-45" size={20} />
             </div>
-            <h1 className="text-3xl lg:text-4xl font-bold bg-gradient-to-r from-indigo-400 to-blue-300 bg-clip-text text-transparent">Stream</h1>
+            <h1 className="text-3xl lg:text-4xl font-bold bg-gradient-to-r from-indigo-400 to-blue-300 bg-clip-text text-transparent md:-mt-1">Stream</h1>
         </div>
     )
 }
