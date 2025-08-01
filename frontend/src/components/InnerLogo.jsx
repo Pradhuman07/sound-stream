3684eaa173c543d3dab03402feb33b2febad80bf
Pradhuman07 <upradyuman@gmail.com>
2025-08-02 02:43:36 +0530

    little more edited, inner logo.. for desktop screens

--- frontend/src/components/InnerLogo.jsx
+++ frontend/src/components/InnerLogo.jsx
@@ -5,8 +5,8 @@ const InnerLogo = () => {
     const navigate = useNavigate()
 
     return (
-        <div onClick={() => navigate('/')} className="flex items-center gap-1.5 lg:gap-2.5 cursor-pointer">
-            <div className="w-7.5 h-7.5 lg:w-8 lg:h-8 bg-gradient-to-r from-blue-300 to-indigo-400 transform rotate-45 transition-transform flex items-center justify-center rounded-full">
+        <div onClick={() => navigate('/')} className="flex items-center gap-1.5 md:gap-2.5 cursor-pointer">
+            <div className="w-7.5 h-7.5 md:w-9 md:h-9 bg-gradient-to-r from-blue-300 to-indigo-400 transform rotate-45 transition-transform flex items-center justify-center rounded-full">
                 <IoMdMusicalNote className="text-white transform -rotate-45" size={20} />
             </div>
             <h1 className="text-3xl lg:text-4xl font-bold bg-gradient-to-r from-indigo-400 to-blue-300 bg-clip-text text-transparent md:-mt-1">Stream</h1>
