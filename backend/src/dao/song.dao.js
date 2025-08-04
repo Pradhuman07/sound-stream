321064354090d3432c400c7b230750f5653ac8dc
Pradhuman07 <upradyuman@gmail.com>
2025-08-05 01:12:02 +0530

    Refactor getAllSongs function

--- backend/src/dao/song.dao.js
+++ backend/src/dao/song.dao.js
@@ -4,12 +4,10 @@ export async function uploadSong(songData) {
     return await songModel.create(songData);
 }
 
-export async function getAllSongs(skip = 0, limit = 12) {
+export async function getAllSongs() {
     return await songModel
         .find()
         .sort({ createdAt: -1 })  // -1 for descending order (newest first)
-        .skip(skip)
-        .limit(limit);
 }
 
 export async function searchSongs(query) {
